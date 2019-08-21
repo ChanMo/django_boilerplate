b8ff9e93dba960b882734114f587083931dd3f9a
dyou <chen.orange@aliyun.com>
2019-08-21 11:22:28 +0800

    use exec instead run --rm

--- fabfile.py
+++ fabfile.py
@@ -8,7 +8,7 @@ from fabric import task
 def deploy(c):
     with c.cd("/path/to/code/"):
         c.run("git pull origin master")
-        c.run("docker-compose -f docker-compose-deploy.yml run --rm web python manage.py makemigrations")
-        c.run("docker-compose -f docker-compose-deploy.yml run --rm web python manage.py migrate --noinput")
-        c.run("docker-compose -f docker-compose-deploy.yml run --rm web python manage.py collectstatic --noinput")
+        c.run("docker-compose -f docker-compose-deploy.yml exec web python manage.py makemigrations")
+        c.run("docker-compose -f docker-compose-deploy.yml exec web python manage.py migrate --noinput")
+        c.run("docker-compose -f docker-compose-deploy.yml exec web python manage.py collectstatic --noinput")
         c.run("docker-compose -f docker-compose-deploy.yml restart")
