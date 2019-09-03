2729c5e431f1a7211aa7d5bb36828bd2f5bb5a71
dyou <chen.orange@aliyun.com>
2019-09-03 11:22:04 +0800

    add local.py for settings

---
+++ local.example.py
@@ -0,0 +1,15 @@
+from web.settings import *
+
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'postgres',
+        'PORT': '5432'
+    }
+}
