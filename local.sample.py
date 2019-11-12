b8fcb67988c35b64ed3ec6ee64de98cd13d5eab4
dyou <chen.orange@aliyun.com>
2019-11-12 15:20:07 +0800

    add cache, loggign

--- local.sample.py
+++ local.sample.py
@@ -1,6 +1,6 @@
 from api.settings import *
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'NO').lower() in ('on', 'true', 'y', 'yes')
 ALLOWED_HOSTS = ['*']
 
 DATABASES = {
@@ -13,3 +13,49 @@ DATABASES = {
         'PORT': '5432'
     }
 }
+
+if not DEBUG:
+    CACHES = {
+        "default": {
+            "BACKEND": "django_redis.cache.RedisCache",
+            "LOCATION": "redis://redis:6380/1",
+            "OPTIONS": {
+                "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            }
+        }
+    }
+
+
+if not DEBUG:
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'console': {
+                'format': '%(name)-12s %(levelname)-8s %(message)s'
+            },
+            'file': {
+                'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+            }
+        },
+        'handlers': {
+            'console': {
+                'class': 'logging.StreamHandler',
+                'formatter': 'console'
+            },
+            'file': {
+                'level': 'DEBUG',
+                'class': 'logging.handlers.TimedRotatingFileHandler',
+                'when': 'midnight',
+                'backupCount': 30,
+                'formatter': 'file',
+                'filename': '/var/log/api/api.log'
+            }
+        },
+        'loggers': {
+            '': {
+                'level': 'DEBUG',
+                'handlers': ['console', 'file']
+            }
+        }
+    }
