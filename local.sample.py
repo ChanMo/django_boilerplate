cf7f19c85fe0849a58bbd5e7356f782c098c1a59
dyou <chen.orange@aliyun.com>
2019-11-12 15:54:35 +0800

    fixed redis port

--- local.sample.py
+++ local.sample.py
@@ -18,7 +18,7 @@ if not DEBUG:
     CACHES = {
         "default": {
             "BACKEND": "django_redis.cache.RedisCache",
-            "LOCATION": "redis://redis:6380/1",
+            "LOCATION": "redis://redis:6379/1",
             "OPTIONS": {
                 "CLIENT_CLASS": "django_redis.client.DefaultClient",
             }
